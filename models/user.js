e21dbaffe4d44fc59176e85095713b8fb568fd27
dumpsterRat92 <you@example.com>
2024-05-14 13:46:28 -0700

    file structure and added to user.js

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
